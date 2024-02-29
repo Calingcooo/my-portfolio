e51f43a87c81c3e61b3a097b54791726061d6319
Calingcooo <calingco.je@gmail.com>
2024-02-29 17:04:58 +0800

    Added navigation

---
+++ src/app/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex items-center bg-gray-200 mt-2 h-20">
+      <div>
+        <img src="#" alt="Profile picture" className="border"/>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/app/components/Navigation.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div className="flex h-16 mb-5">
+      <div className="flex items-center justify-center w-full">
+        <div className="flex justify-evenly items-center bg-black bg-opacity-70 text-white h-full w-1/4 rounded-full">
+          <span className="rounded-full px-4 py-2 bg-gray-400 text-black font-bold hover:bg-gray-200 cursor-pointer hover:-translate-y-2 duration-300 transition-all">
+            1
+          </span>
+          <span className="rounded-full px-4 py-2 bg-gray-400 text-black font-bold hover:bg-gray-200 cursor-pointer hover:-translate-y-2 duration-300 transition-all">
+            2
+          </span>
+          <span className="rounded-full px-4 py-2 bg-gray-400 text-black font-bold hover:bg-gray-200 cursor-pointer hover:-translate-y-2 duration-300 transition-all">
+            3
+          </span>
+          <span className="rounded-full px-4 py-2 bg-gray-400 text-black font-bold hover:bg-gray-200 cursor-pointer hover:-translate-y-2 duration-300 transition-all">
+            4
+          </span>
+          <span className="rounded-full px-4 py-2 bg-gray-400 text-black font-bold hover:bg-gray-200 cursor-pointer hover:-translate-y-2 duration-300 transition-all">
+            5
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
